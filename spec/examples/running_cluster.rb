0a5635649d8792a5b40751d5fcc7c1890096c8a3
Morgan Rhodes <morgan@puppet.com>
2019-03-21 11:46:14 -0700

    (maint) try with a custom consul

--- spec/examples/running_cluster.rb
+++ spec/examples/running_cluster.rb
@@ -79,6 +79,7 @@ shared_examples 'a running pupperware cluster' do
     # setting up a Windows TTY is difficult, so we don't
     # allocating a TTY will show container pull output on Linux, but that's not good for tests
     consul_ip = run_command("docker-compose --no-ansi exec -T consul ifconfig eth0 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1")[:stdout].chomp
+    puts "CONSUL IP! #{consul_ip}"
     result = run_command("docker run --rm --network pupperware_default --dns #{consul_ip} --name #{agent_name} --hostname #{agent_name} puppet/puppet-agent-alpine agent -t --server puppet.service.consul --ca_server puppet")
     return result[:status].exitstatus
   end
