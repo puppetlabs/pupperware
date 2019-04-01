8b73dfd6ceb38babe3303e56d157e0dcb13eed17
Iristyle <Iristyle@github>
2019-04-01 15:57:21 -0700

    (maint) Move generic container methods to Helpers
     - This code really should be moved to the centralized helper file

--- spec/examples/running_cluster.rb
+++ spec/examples/running_cluster.rb
@@ -6,49 +6,6 @@ shared_examples 'a running pupperware cluster' do
 
   include Helpers
 
-  def inspect_container(container, query)
-    result = run_command("docker inspect \"#{container}\" --format \"#{query}\"")
-    status = result[:stdout].chomp
-    STDOUT.puts "queried #{query} of #{container}: #{status}"
-    return status
-  end
-
-  def get_container_status(container)
-    inspect_container(container, '{{.State.Health.Status}}')
-  end
-
-  def get_service_container(service, timeout = 120)
-    result = run_command("docker-compose --no-ansi ps --quiet #{service}")
-    container = result[:stdout].chomp
-    Timeout::timeout(timeout) do
-      while container.empty?
-        sleep(1)
-        result = run_command("docker-compose --no-ansi ps --quiet #{service}")
-        container = result[:stdout].chomp
-      end
-    end
-
-    STDOUT.puts("service named '#{service}' is hosted in container: '#{container}'")
-    return container
-  rescue Timeout::Error
-    msg = "docker-compose never started a service named '#{service}'"
-    STDOUT.puts(msg)
-    raise msg
-  end
-
-  def get_service_base_uri(service, port)
-    @mapped_ports["#{service}:#{port}"] ||= begin
-      result = run_command("docker-compose --no-ansi port #{service} #{port}")
-      service_ip_port = result[:stdout].chomp
-      raise "Could not retrieve service endpoint for #{service}:#{port}" if service_ip_port == ''
-      uri = URI("http://#{service_ip_port}")
-      uri.host = 'localhost' if uri.host == '0.0.0.0'
-      STDOUT.puts "determined #{service} endpoint for port #{port}: #{uri}"
-      uri
-    end
-    @mapped_ports["#{service}:#{port}"]
-  end
-
   def get_puppetdb_state
     # make sure PDB container hasn't stopped
     get_service_container('puppetdb', 5)
