fc5a5b153cda0ac5f68762b0f05830793a35201c
Iristyle <Iristyle@github>
2019-08-26 11:03:05 -0700

    (maint) Reformat run_agent helper
     - Long line lengths are making this hard to read

--- gem/lib/pupperware/spec_helper.rb
+++ gem/lib/pupperware/spec_helper.rb
@@ -473,14 +473,22 @@ module SpecHelpers
   # Two issues related to this issue are
   # https://github.com/docker/libnetwork/issues/2371 and
   # https://github.com/Microsoft/opengcs/issues/303
-  def run_agent(agent_name, network, server: get_container_hostname(get_service_container('puppet')), ca: get_container_hostname(get_service_container('puppet')), masterport: 8140, ca_port: nil)
+  def run_agent(
+    agent_name,
+    network,
+    server: get_container_hostname(get_service_container('puppet')),
+    ca: get_container_hostname(get_service_container('puppet')),
+    masterport: 8140,
+    ca_port: nil)
     # default ca_port to masterport if unset
     ca_port = masterport if ca_port.nil?
 
     # setting up a Windows TTY is difficult, so we don't
     # allocating a TTY will show container pull output on Linux, but that's not good for tests
     STDOUT.puts("running agent #{agent_name} in network #{network} against #{server}")
-    result = run_command("docker run --rm --network #{network} --name #{agent_name} --hostname #{agent_name} puppet/puppet-agent-ubuntu agent --verbose --onetime --no-daemonize --summarize --server #{server} --masterport #{masterport} --ca_server #{ca} --ca_port #{ca_port}")
+    result = run_command("docker run --rm --network #{network} --name #{agent_name} --hostname #{agent_name} \
+      puppet/puppet-agent-ubuntu agent --verbose --onetime --no-daemonize --summarize \
+      --server #{server} --masterport #{masterport} --ca_server #{ca} --ca_port #{ca_port}")
     return result[:status].exitstatus
   end
 
