d4143458269f159a76b222854fa3619bf13335b8
Nate Wolfe <nwolfe@puppet.com>
2019-08-30 15:27:21 -0700

    (MAINT) Move pe-postgres to new repo
    Everything has been moved over to
    https://github.com/puppetlabs/docker-pe-postgres

--- commercial/pe-postgres/postgres-custom/00-envsubst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# NOTE: define ENV variables and defaults in the Dockerfile
-
-for f in /etc/postgresql/config/*; do
-    envsubst < "$f" > "${f}.new"
-    mv --force "${f}.new" "$f"
-done
