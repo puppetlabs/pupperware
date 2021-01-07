d936f4b9da4d50bf87e2516e2c77cf0f0e1b1525
Florent Poinsaut <floSoX@users.noreply.github.com>
2021-01-07 23:11:55 +0100

    Add a shim for the r10k CLI

---
+++ bin/r10k.ps1
@@ -0,0 +1,9 @@
+[CmdletBinding()]
+param (
+  [parameter(Mandatory=$False,Position=0,ValueFromRemainingArguments=$True)]
+  [Object[]] $Arguments
+)
+
+Push-Location (Join-Path -Path $PSScriptRoot -ChildPath '..') | Out-Null
+
+& docker-compose exec puppet r10k $Arguments
